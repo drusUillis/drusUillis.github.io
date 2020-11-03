dba7ee317b57d28b2ea9db473b629f130a19abe9
Andrey <vahrameev007@gmail.com>
2020-11-03 21:58:24 +0300

    почти финал

--- js/script1.js
+++ js/script1.js
@@ -1,33 +1,49 @@
+function dobav(chislo) 
+{
+    var chislo = document.getElementById("chislo").value;
+	var resultat = chislo+" было введено.";
+	document.getElementById("resultat").innerHTML = resultat;
+}
+function check() {
+        	var check_text = document.getElementById("check_text");
+          let name = document.getElementById('name').value
+          document.getElementById('profile_name').textContent = name
 
+          let sex = document.getElementById('sex').value
+          document.getElementById('profile_sex').textContent = sex
 
-function calcD(a, b, c)
+          let age = document.getElementById('age').value
+          document.getElementById('profile_age').textContent = age
+        }
+function calcSum(a, b)
 {
 	var a = Number(document.getElementById("a").value);
     var b = Number(document.getElementById("b").value);
-	var c = Number(document.getElementById("c").value);
-    var result = b*b - 4*a*c;
+    var result = a + b;
 	document.getElementById("result").innerHTML = result;
 }
-function Min(e, d)
+function massiv(v,h,n,m,j)
 {
-	var e = Number(document.getElementById("e").value);
-    var d = Number(document.getElementById("d").value);
-	if (e<d) {
-		e = e;	
+	var v = (document.getElementById("v").value);
+	var h = (document.getElementById("h").value);
+	var n = (document.getElementById("n").value);
+	var m = (document.getElementById("m").value);
+	var j = (document.getElementById("j").value);
+	var arr=[v,h,n,m,j];
+	for (var i= 0; i < arr.length; i++) {
+		if(Number(arr[i])){
+			arr[i]=arr[i]*arr[i];
+			
+		}   
+		else{
+			arr[i]="0";
+		} 
 		
 	}
-	else {
-		e = d;
-	}
-	var result1 = e;
-	document.getElementById("result1").innerHTML = result1;
+	alert (arr);
 }
-function vozrast() 
-{
-		var age = prompt('Сколько вам лет?','Введите сюда ваш возраст');
-		if (!confirm('Вы уверены, что вам ' + age + ' лет?'))
-		{
-		vozrast()
-		}
+function changeColor(colorValue) {
+    document.body.style.background =     document.getElementById(colorValue).dataset.color;
 }
+
 
